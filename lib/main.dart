d37b980b5cbd48163cd051e033b83029abf14c26
hhumbetovv <hhumbetovv@gmail.com>
2023-02-01 15:36:49 +0400

    color themes added

--- lib/main.dart
+++ lib/main.dart
@@ -20,12 +20,13 @@ class _ExaminationState extends State<Examination> {
   @override
   Widget build(BuildContext context) {
     final List<Subject> subjects = Subject.subjects;
+    final AppTheme appTheme = AppTheme();
 
     return MaterialApp(
       title: 'Examination',
-      theme: lightTheme,
-      darkTheme: darkTheme,
-      themeMode: mode,
+      theme: appTheme.light(AppColors.colors[0]),
+      darkTheme: appTheme.dark(AppColors.colors[0]),
+      themeMode: ThemeMode.light,
       debugShowCheckedModeBanner: false,
       home: subjects.length == 1 ? SelectMode(subject: subjects[0]) : SelectSubject(subjects: subjects),
     );
--- lib/theme.dart
+++ lib/theme.dart
@@ -2,224 +2,209 @@ import 'package:flutter/material.dart';
 
 import 'constants.dart';
 
-final ColorTheme selectedColor = ColorTheme.colors[0];
-final ThemeData lightTheme = ThemeData(
-  //! Brightness
-  brightness: Brightness.light,
-  //! Color Scheme
-  colorScheme: ColorScheme.light(
-    primary: selectedColor.light.primary,
-    secondary: selectedColor.light.secondary,
-    background: selectedColor.light.background,
-    onPrimary: Colors.black,
-    onSecondary: Colors.white,
-    tertiary: Colors.black,
-  ),
-  //! Scaffold Background Color
-  scaffoldBackgroundColor: selectedColor.light.background,
-  //! AppBar Theme
-  appBarTheme: AppBarTheme(
-    iconTheme: const IconThemeData(
-      color: Colors.black,
-    ),
-    color: selectedColor.light.primary,
-    centerTitle: true,
-    shape: const RoundedRectangleBorder(
-      borderRadius: BorderRadius.vertical(
-        bottom: Radius.circular(15),
-      ),
-    ),
-    titleTextStyle: const TextStyle(
-      fontSize: Constants.fontSizeSmall,
-      color: Colors.black,
-      overflow: TextOverflow.ellipsis,
-    ),
-  ),
-  //! Elevated Button Theme
-  elevatedButtonTheme: ElevatedButtonThemeData(
-    style: ElevatedButton.styleFrom(
-      backgroundColor: selectedColor.light.primary,
-      textStyle: const TextStyle(
-        fontSize: Constants.fontSizeMedium,
-        color: Colors.black,
-      ),
-      minimumSize: const Size.fromHeight(50),
-      shape: RoundedRectangleBorder(
-        borderRadius: Constants.radiusSmall,
-      ),
-    ),
-  ),
-  //! Bottom AppBar Theme
-  bottomAppBarTheme: BottomAppBarTheme(
-    shape: const CircularNotchedRectangle(),
-    color: selectedColor.light.primary,
-  ),
-  //! Icon Theme
-  iconTheme: const IconThemeData(
-    color: Colors.black,
-  ),
-  //! Floating Action Button Theme
-  floatingActionButtonTheme: FloatingActionButtonThemeData(
-    backgroundColor: selectedColor.light.secondary,
-    extendedTextStyle: const TextStyle(
-      color: Colors.white,
-      fontSize: Constants.fontSizeSmall,
-    ),
-  ),
-  //! Highlight and Splash Colors
-  highlightColor: Colors.black.withOpacity(0.2),
-  splashColor: Colors.black.withOpacity(0.2),
-  //! Text Theme
-  textTheme: const TextTheme(
-    bodyText2: TextStyle(
-      fontSize: Constants.fontSizeSmall,
-      color: Colors.black,
-    ),
-  ),
-  //! Progress Indicator Theme
-  progressIndicatorTheme: ProgressIndicatorThemeData(
-    color: selectedColor.light.secondary,
-  ),
-  //! Divider Theme
-  dividerTheme: DividerThemeData(
-    color: selectedColor.light.secondary,
-    thickness: 1,
-  ),
-  //! Checkbox Theme
-  checkboxTheme: CheckboxThemeData(
-    fillColor: MaterialStateProperty.all<Color>(selectedColor.light.secondary),
-  ),
-  //! Radio Theme
-  radioTheme: RadioThemeData(
-    fillColor: MaterialStateProperty.all<Color>(selectedColor.light.secondary),
-  ),
-  //! Text Input Decoration Theme
-  inputDecorationTheme: InputDecorationTheme(
-    errorStyle: const TextStyle(
-      height: 0,
-    ),
-    focusedBorder: UnderlineInputBorder(
-      borderSide: BorderSide(
-        color: selectedColor.light.secondary,
-      ),
-    ),
-  ),
-  //! Dialog Theme
-  dialogTheme: DialogTheme(
-    shape: RoundedRectangleBorder(
-      borderRadius: Constants.radiusLarge,
-    ),
-    backgroundColor: selectedColor.light.background,
-  ),
-);
+class AppColors {
+  final Color primaryDark;
+  final Color primaryLight;
+  final Color backgroundDark;
+  final Color backgroundLight;
 
-final ThemeData darkTheme = ThemeData(
-  //! Brightness
-  brightness: Brightness.dark,
-  //! Color Scheme
-  colorScheme: ColorScheme.dark(
-    primary: selectedColor.dark.primary,
-    secondary: selectedColor.dark.secondary,
-    background: selectedColor.dark.background,
-    onPrimary: Colors.white,
-    onSecondary: Colors.black,
-    tertiary: Colors.white,
-  ),
-  //! Scaffold Background Color
-  scaffoldBackgroundColor: selectedColor.dark.background,
-  //! AppBar Theme
-  appBarTheme: AppBarTheme(
-    iconTheme: const IconThemeData(
-      color: Colors.white,
+  AppColors(
+    this.primaryDark,
+    this.primaryLight,
+    this.backgroundDark,
+    this.backgroundLight,
+  );
+
+  static List<AppColors> colors = [
+    // AppColors(
+    //   Color(0xFF),
+    //   Color(0xFF),
+    //   Color(0xFF),
+    //   Color(0xFF),
+    // ),
+
+    //! S - 90 => 50 V - 50 => 90
+    AppColors(
+      const Color(0xFF7f0c0c),
+      const Color(0xFFe57272),
+      const Color(0xFF190f14),
+      const Color(0xFFe5ceda),
     ),
-    color: selectedColor.dark.primary,
-    centerTitle: true,
-    shape: const RoundedRectangleBorder(
-      borderRadius: BorderRadius.vertical(
-        bottom: Radius.circular(15),
+    AppColors(
+      const Color(0xFF7f0c7f),
+      const Color(0xFFe572e5),
+      const Color(0xFF190f14),
+      const Color(0xFFe5ceda),
+    ),
+    AppColors(
+      const Color(0xFF460c7f),
+      const Color(0xFFac72e5),
+      const Color(0xFF0f1019),
+      const Color(0xFFe5e8ff),
+    ),
+    //! S - 50 V - 40 => 90
+    AppColors(
+      const Color(0xFF333366),
+      const Color(0xFF7272e5),
+      const Color(0xFF0f1619),
+      const Color(0xFFcedde5),
+    ),
+    AppColors(
+      const Color(0xFF336666),
+      const Color(0xFF72e5e5),
+      const Color(0xFF0f1914),
+      const Color(0xFFcee5da),
+    ),
+    AppColors(
+      const Color(0xFF336633),
+      const Color(0xFF72e572),
+      const Color(0xFF14190f),
+      const Color(0xFFdae5ce),
+    ),
+    //! S - 90 => 70 V - 90 => 100
+    AppColors(
+      const Color(0xFFe5b116),
+      const Color(0xFFffd24c),
+      const Color(0xFF19140f),
+      const Color(0xFFe5dace),
+    ),
+    AppColors(
+      const Color(0xFFe55b16),
+      const Color(0xFFff884c),
+      const Color(0xFF19110f),
+      const Color(0xFFe5d2ce),
+    ),
+    AppColors(
+      const Color(0xFF3d3d3d),
+      const Color(0xFFb2b2b2),
+      const Color(0xFF161616),
+      const Color(0xFFe5e5e5),
+    ),
+  ];
+}
+
+class AppTheme {
+  ThemeData light(AppColors theme) => _getTheme(theme, false);
+  ThemeData dark(AppColors theme) => _getTheme(theme, true);
+
+  ThemeData _getTheme(AppColors theme, bool isDark) {
+    return ThemeData(
+      //! Brightness
+      brightness: isDark ? Brightness.dark : Brightness.light,
+      //! Color Scheme
+      colorScheme: isDark
+          ? ColorScheme.dark(
+              primary: theme.primaryDark,
+              secondary: theme.primaryLight,
+              background: theme.backgroundDark,
+              onPrimary: Colors.white,
+              onSecondary: Colors.black,
+              tertiary: Colors.white,
+            )
+          : ColorScheme.light(
+              primary: theme.primaryLight,
+              secondary: theme.primaryDark,
+              background: theme.backgroundLight,
+              onPrimary: Colors.black,
+              onSecondary: Colors.white,
+              tertiary: Colors.black,
+            ),
+      //! Scaffold Background Color
+      scaffoldBackgroundColor: isDark ? theme.backgroundDark : theme.backgroundLight,
+      //! AppBar Theme
+      appBarTheme: AppBarTheme(
+        centerTitle: true,
+        titleTextStyle: TextStyle(
+          fontSize: Constants.fontSizeSmall,
+          color: isDark ? Colors.white : Colors.black,
+          overflow: TextOverflow.ellipsis,
+        ),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(15),
+          ),
+        ),
+        color: isDark ? theme.primaryDark : theme.primaryLight,
+        iconTheme: IconThemeData(
+          color: isDark ? Colors.white : Colors.black,
+        ),
       ),
-    ),
-    titleTextStyle: const TextStyle(
-      fontSize: Constants.fontSizeSmall,
-      color: Colors.white,
-      overflow: TextOverflow.ellipsis,
-    ),
-  ),
-  //! Elevated Button Theme
-  elevatedButtonTheme: ElevatedButtonThemeData(
-    style: ElevatedButton.styleFrom(
-      backgroundColor: selectedColor.dark.primary,
-      textStyle: const TextStyle(
-        fontSize: Constants.fontSizeMedium,
-        color: Colors.white,
+      //! Elevated Button Theme
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          minimumSize: const Size.fromHeight(50),
+          backgroundColor: isDark ? theme.primaryDark : theme.primaryLight,
+          textStyle: TextStyle(
+            fontSize: Constants.fontSizeMedium,
+            color: isDark ? Colors.white : Colors.black,
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: Constants.radiusSmall,
+          ),
+        ),
       ),
-      minimumSize: const Size.fromHeight(50),
-      shape: RoundedRectangleBorder(
-        borderRadius: Constants.radiusSmall,
+      //! Bottom AppBar Theme
+      bottomAppBarTheme: BottomAppBarTheme(
+        shape: const CircularNotchedRectangle(),
+        color: isDark ? theme.primaryDark : theme.primaryLight,
       ),
-    ),
-  ),
-  //! Bottom AppBar Theme
-  bottomAppBarTheme: BottomAppBarTheme(
-    shape: const CircularNotchedRectangle(),
-    color: selectedColor.dark.primary,
-  ),
-  //! Icon Theme
-  iconTheme: const IconThemeData(
-    color: Colors.white,
-  ),
-  //! Floating Action Button Theme
-  floatingActionButtonTheme: FloatingActionButtonThemeData(
-    backgroundColor: selectedColor.dark.secondary,
-    extendedTextStyle: const TextStyle(
-      color: Colors.black,
-      fontSize: Constants.fontSizeSmall,
-    ),
-  ),
-  //! Highlight and Splash Colors
-  highlightColor: Colors.white.withOpacity(0.2),
-  splashColor: Colors.white.withOpacity(0.2),
-  //! Text Theme
-  textTheme: const TextTheme(
-    bodyText2: TextStyle(
-      fontSize: Constants.fontSizeSmall,
-      color: Colors.white,
-    ),
-  ),
-  //! Progress Indicator Theme
-  progressIndicatorTheme: ProgressIndicatorThemeData(
-    color: selectedColor.dark.secondary,
-  ),
-  //! Divider Theme
-  dividerTheme: DividerThemeData(
-    color: selectedColor.dark.secondary,
-    thickness: 1,
-  ),
-  //! Checkbox Theme
-  checkboxTheme: CheckboxThemeData(
-    fillColor: MaterialStateProperty.all<Color>(selectedColor.dark.secondary),
-  ),
-  //! Radio Theme
-  radioTheme: RadioThemeData(
-    fillColor: MaterialStateProperty.all<Color>(selectedColor.dark.secondary),
-  ),
-  //! Text Input Decoration Theme
-  inputDecorationTheme: InputDecorationTheme(
-    errorStyle: const TextStyle(
-      height: 0,
-    ),
-    focusedBorder: UnderlineInputBorder(
-      borderSide: BorderSide(
-        color: selectedColor.dark.secondary,
+      //! Icon Theme
+      iconTheme: IconThemeData(
+        color: isDark ? Colors.white : Colors.black,
       ),
-    ),
-  ),
-  //! Dialog Theme
-  dialogTheme: DialogTheme(
-    shape: RoundedRectangleBorder(
-      borderRadius: Constants.radiusLarge,
-    ),
-    backgroundColor: selectedColor.dark.background,
-  ),
-);
-const ThemeMode mode = ThemeMode.dark;
+      //! Floating Action Button Theme
+      floatingActionButtonTheme: FloatingActionButtonThemeData(
+        backgroundColor: isDark ? theme.primaryLight : theme.primaryDark,
+        extendedTextStyle: TextStyle(
+          color: isDark ? Colors.black : Colors.white,
+          fontSize: Constants.fontSizeSmall,
+        ),
+      ),
+      //! Highlight and Splash Colors
+      highlightColor: isDark ? Colors.white.withOpacity(0.2) : Colors.black.withOpacity(0.2),
+      splashColor: isDark ? Colors.white.withOpacity(0.2) : Colors.black.withOpacity(0.2),
+      //! Text Theme
+      textTheme: TextTheme(
+        bodyText2: TextStyle(
+          fontSize: Constants.fontSizeSmall,
+          color: isDark ? Colors.white : Colors.black,
+        ),
+      ),
+      //! Progress Indicator Theme
+      progressIndicatorTheme: ProgressIndicatorThemeData(
+        color: isDark ? theme.primaryLight : theme.primaryDark,
+      ),
+      //! Divider Theme
+      dividerTheme: DividerThemeData(
+        color: isDark ? theme.primaryLight : theme.primaryDark,
+        thickness: 1,
+      ),
+      //! Checkbox Theme
+      checkboxTheme: CheckboxThemeData(
+        fillColor: MaterialStateProperty.all<Color>(isDark ? theme.primaryLight : theme.primaryDark),
+      ),
+      //! Radio Theme
+      radioTheme: RadioThemeData(
+        fillColor: MaterialStateProperty.all<Color>(isDark ? theme.primaryLight : theme.primaryDark),
+      ),
+      //! Text Input Decoration Theme
+      inputDecorationTheme: InputDecorationTheme(
+        errorStyle: const TextStyle(
+          height: 0,
+        ),
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: isDark ? theme.primaryLight : theme.primaryDark,
+          ),
+        ),
+      ),
+      //! Dialog Theme
+      dialogTheme: DialogTheme(
+        shape: RoundedRectangleBorder(
+          borderRadius: Constants.radiusLarge,
+        ),
+        backgroundColor: isDark ? theme.backgroundDark : theme.backgroundLight,
+      ),
+    );
+  }
+}
